5b57a9c337cf095303332cb13a94296ef0f3df0c
Melody_Yuuka <a876987@126.com>
2024-07-11 23:41:46 +0800

    upload resources

---
+++ manual/sh03c67/ja/data/chgimg.js
@@ -0,0 +1,16 @@
+<!--
+if(document.images){
+	var Img_file = new Array();
+	Img_file[0] = new Image(); Img_file[0].src = "image/shortcut1.gif";
+	Img_file[1] = new Image(); Img_file[1].src = "image/shortcut2.gif";
+}
+function ChageImage(id,ImgName){
+	if(document.images){
+		if(Img_file[ImgName]){
+			document.images[id].src = Img_file[ImgName].src;
+		}else{
+			document.images[id].src = ImgName;
+		}
+	}
+}
+// -->
